e44991ce3cbe1891fcc2563577c6249f4abb6767
Seebbe <55194973+Seebbe@users.noreply.github.com>
2020-10-19 08:33:37 +0200

    javaFil
    Java

---
+++ java/ScheduleApp/src/main/java/com/project/scheduleapp/demo/ScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.scheduleapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAppApplication.class, args);
+    }
+
+}
---
+++ java/ScheduleApp/src/test/java/com/project/scheduleapp/demo/ScheduleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.scheduleapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
