23fcdb6683b18fe1d427b47670e5ed6dc776a35a
Seebbe <55194973+Seebbe@users.noreply.github.com>
2020-10-23 09:34:10 +0200

    CategoryCrud
    CategoryCrud

---
+++ java/ScheduleApp/src/main/java/com/project/scheduleapp/demo/Repository/ICategoryCrud.java
@@ -0,0 +1,8 @@
+package com.project.scheduleapp.demo.Repository;
+
+import com.project.scheduleapp.demo.Model.Category;
+
+public interface ICategoryCrud {
+
+    public Category addCategory(Category category);
+}
